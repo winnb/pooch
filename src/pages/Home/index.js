6a8078ce36fd45f7259b50aef0bba6af25f7938a
Brandon Winn <b_winn2@yahoo.com>
2020-01-23 15:24:57 -0800

    BW: Added pictures to home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,18 @@
 // React
 import React from "react";
 
-//Firebase
-import Fire from "../../config/Fire.js";
-
 //Grahpics, Styles, Animation
-import Paw from "./media/paw-icon.png";
-import Walking from "./media/walking-icon.png";
-import Doghouse from "./media/doghouse-icon.png";
+// import Paw from "./media/paw-icon.png";
+// import Walking from "./media/walking-icon.png";
+// import Doghouse from "./media/doghouse-icon.png";
 import "./styles.scss";
 import Slide from "react-reveal";
+import FamilyPets from './media/familypets.jpg';
+import DogWalking from './media/dogwalking.jpg';
+import DogBoarding from './media/dogboarding.jpg';
+import DogGrooming from './media/grooming.jpg';
+import DogPark from './media/dogpark.jpg';
+import Vet from './media/vet.jpg';
 
 // Components
 import Slideshow from "../../components/Slideshow";
@@ -18,10 +21,6 @@ class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "",
-      phone: "",
-      hourlyRate: "",
-      city: ""
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -29,89 +28,6 @@ class Home extends React.Component {
 
   handleChange(e) {
     this.setState({ [e.target.name]: e.target.value });
-  }
-
-  componentDidMount() {
-    // New Walker Table
-    const db = Fire.firestore();
-    // get the data from the collection
-    db.collection("walkers")
-    .orderBy("timestamp", "desc")
-    .limit(3)
-    .get()
-    .then(snapshot => {
-      snapshot.docs.forEach(doc => {
-        renderWalkers(doc);
-      });
-    });
-     // Create a grid to store walker data
-     const grid = document.querySelector("#walker-grid");
-     function renderWalkers(doc) {
-       // create a list to style the data
-       let li = document.createElement("tr");
-       let name = document.createElement("td");
-       let phone = document.createElement("td");
-       let hourlyRate = document.createElement("td");
-       let city = document.createElement("td");
- 
-       li.setAttribute("data-id", doc.id);
- 
-       // Update local state to database contents
-       name.textContent = doc.data().name;
-       phone.textContent = doc.data().phone;
-       hourlyRate.textContent = doc.data().hourlyRate;
-       city.textContent = doc.data().city;
- 
-       // Add all contents to the list
-       li.appendChild(name);
-      //  li.appendChild(phone);
-      //  li.appendChild(hourlyRate);
-       li.appendChild(city);
- 
-       // Add list to grid
-       grid.appendChild(li);
-     }
-
-     // New Boarder Table
-     db.collection("boarders")
-    .orderBy("name", "desc")
-    .limit(3)
-    .get()
-    .then(snapshot => {
-      snapshot.docs.forEach(doc => {
-        renderBoarders(doc);
-      });
-    });
-     // Create a grid to store walker data
-     const boarderGrid = document.querySelector("#boarder-grid");
-     function renderBoarders(doc) {
-       // create a list to style the data
-       let li = document.createElement("tr");
-       let name = document.createElement("td");
-       let phone = document.createElement("td");
-       let dailyRate = document.createElement("td");
-       let address = document.createElement("td");
-       let city = document.createElement("td");
- 
-       li.setAttribute("data-id", doc.id);
- 
-       // Update local state to database contents
-       name.textContent = doc.data().name;
-       phone.textContent = doc.data().phone;
-       dailyRate.textContent = doc.data().dailyRate;
-       address.textContent = doc.data().address;
-       city.textContent = doc.data().city;
- 
-       // Add all contents to the list
-       li.appendChild(name);
-      //  li.appendChild(phone);
-      //  li.appendChild(dailyRate);
-      // li.appendChild(address);
-       li.appendChild(city);
- 
-       // Add list to grid
-       boarderGrid.appendChild(li);
-     }
   }
 
   render() {
@@ -123,22 +39,22 @@ class Home extends React.Component {
         <div className="row mb-5 mt-3">
           <Slide left>
             <div className="row my-5" id="section2">
-              <div className="col mx-5">
+              <div className="col mx-5 mb-7">
                 Paragraph 1 will go here. This paragraph will talk about the features of the Your Pets page. 
                 Paragraph 1 will go here. This paragraph will talk about the features of the Your Pets page. 
                 Paragraph 1 will go here. This paragraph will talk about the features of the Your Pets page. 
               </div>
               <div className="col mx-5 my-5">
-                Image 1
+                <img src={FamilyPets} alt="Family pets"/>
               </div>
             </div>
             </Slide>
             <Slide right>
             <div className="row my-5" id="section3">
               <div className="col mx-5 my-5">
-                Image 2
+                <img src={DogWalking} alt="Walking dogs"/>
               </div>
-              <div className="col mx-5">
+              <div className="col mx-5 mb-7">
                 Paragraph 2 will go here. This paragraph will talk about the features of the Dog Walking page.  
                 Paragraph 2 will go here. This paragraph will talk about the features of the Dog Walking page.  
                 Paragraph 2 will go here. This paragraph will talk about the features of the Dog Walking page.   
@@ -147,22 +63,22 @@ class Home extends React.Component {
             </Slide>
             <Slide left>
             <div className="row my-5" id="section4">
-              <div className="col mx-5">
+              <div className="col mx-5 mb-7">
                 Paragraph 3 will go here. This paragraph will talk about the features of the Dog Boarding page.  
                 Paragraph 3 will go here. This paragraph will talk about the features of the Dog Boarding page.  
                 Paragraph 3 will go here. This paragraph will talk about the features of the Dog Boarding page.  
               </div>
               <div className="col mx-5 my-5">
-                Image 3
+                <img src={DogBoarding} alt="Dog boarding"/>
               </div>
             </div>
             </Slide>
             <Slide right>
             <div className="row my-5" id="section5">
               <div className="col mx-5 my-5">
-                Image 4
+                <img src={DogGrooming} alt="Dog grooming"/>
               </div>
-              <div className="col mx-5">
+              <div className="col mx-5 mb-7">
                 Paragraph 4 will go here. This paragraph will talk about the features of the Dog Services page.  
                 Paragraph 4 will go here. This paragraph will talk about the features of the Dog Services page.  
                 Paragraph 4 will go here. This paragraph will talk about the features of the Dog Services page.  
@@ -171,22 +87,22 @@ class Home extends React.Component {
             </Slide>
             <Slide left>
             <div className="row my-5" id="section6">
-              <div className="col mx-5">
+              <div className="col mx-5 mb-7">
                 Paragraph 5 will go here. This paragraph will talk about the features of the Dog Meetup page.  
                 Paragraph 5 will go here. This paragraph will talk about the features of the Dog Meetup page.  
                 Paragraph 5 will go here. This paragraph will talk about the features of the Dog Meetup page.  
               </div>
               <div className="col mx-5 my-5">
-                Image 5
+                <img src={DogPark} alt="Dog park"/>
               </div>
             </div>
             </Slide>
             <Slide right>
             <div className="row my-5" id="section7">
               <div className="col mx-5 my-5">
-                Image 6
+                <img src={Vet} alt="Vet visit"/>
               </div>
-              <div className="col mx-5">
+              <div className="col mx-5 mb-7">
                 Paragraph 6 will go here. This paragraph will talk about the features of the Dog Records page. 
                 Paragraph 6 will go here. This paragraph will talk about the features of the Dog Records page. 
                 Paragraph 6 will go here. This paragraph will talk about the features of the Dog Records page. 
