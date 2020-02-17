f4cefb51ec3e53587d0afffdef42a0eb580b9224
Brandon Winn <b_winn2@yahoo.com>
2020-02-17 15:51:27 -0800

    BW: Upload profile pictures and upload records

--- src/components/FileUpload/index.js
+++ src/components/FileUpload/index.js
@@ -1,30 +1,49 @@
 // React
 import React from "react";
-// Tools
-import Axios from 'axios';
+//Firebase
+import Fire from "../../config/Fire.js";
+// Styling
+import "./styles.scss";
 
 class FileUpload extends React.Component{
-    state={
-        selectedFile: null
+
+    componentDidMount() {
+        const db = Fire.firestore();
+        setTimeout(() => { // Show all uploaded records
+            db.collection("records")
+            .where("email", "==", Fire.auth().currentUser.email)
+            .get()
+            .then(snapshot => {
+              snapshot.docs.forEach(doc => {
+                renderRecords(doc);
+              });
+            });
+          }, 1750);
+          function renderRecords(doc) {
+              document.getElementById("record-row").innerHTML += "<img id=\"record\" className=\"mx-2\" src="+doc.data().pic+" />";
+          }
     }
 
-    fileSelectedHandler = event => {
-        this.setState({
-            selectedFile: event.target.files[0]
-        })
+    filePreview = event => {
+        var input = event.target; // Get image that changed state of input element
+        var reader = new FileReader();
+        reader.onload = function() {
+        var dataURL = reader.result;
+        var output = document.getElementById('preview');
+        output.src = dataURL;
+        };
+        reader.readAsDataURL(input.files[0]); // Show preview of image
     }
 
-    fileUploadHndler = () => {
-        Axios.post('')
-    }
-
-    toggleCollapse() {
-        if (document.getElementById("profileDropdown").className === "collapse") {
-            document.getElementById("profileDropdown").className = "collapse.show";
-        }
-        else if (document.getElementById("profileDropdown").className === "collapse.show") {
-            document.getElementById("profileDropdown").className = "collapse"; 
-        }
+    fileUpload() {
+        const db = Fire.firestore(); // Save image to database
+        db.collection("records").add({
+            email: Fire.auth().currentUser.email,
+            pic: document.getElementById("preview").src
+        });
+        setTimeout(() => {
+            window.location.reload();
+          }, 1000);
     }
 
     render(){
@@ -33,9 +52,19 @@ class FileUpload extends React.Component{
                 <div className="trak_heading-medium mb-5">
                     Dog Records
                 </div>
-                <div className="Upload mb-5">
-                    <input type="file" onChange={this.fileSelectedHandler} />
-                    <button onClick={this.fileUploadHandler}>Upload Dog Record</button>   
+                <div className="Upload mb-5 col mx-10">
+                    <div className="row my-2">
+                        <input type="file" id="file-input" onChange={this.filePreview} />
+                    </div>
+                    <div className="row">
+                        <img id="preview"/>
+                    </div>
+                    <div className="row my-2">
+                        <button onClick={this.fileUpload}>Upload Dog Record</button>
+                    </div>
+                    <div className="row" id="record-row">
+                        <img id="record-pic"/>
+                    </div>
                 </div>
             </div>
         );
