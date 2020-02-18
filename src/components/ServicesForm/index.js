9f0f420a889fdad9c668e69209e99ca7a29b161c
Shashi Kumar Kadari Mallikarjuna <shashikumar1998@gmail.com>
2020-02-18 09:21:05 -0800

    cordinates update

--- src/components/ServicesForm/index.js
+++ src/components/ServicesForm/index.js
@@ -3,7 +3,33 @@ import React from "react";
 // Styles
 import Slide from "react-reveal";
 
+
 class ServicesForm extends React.Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+    };
+  }
+  componentDidMount() {
+    setInterval(() => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.updatePostion(position);
+      },
+       (error) => {},
+       {enableHighAccuracy: true, timeout: 20000, maximumAge: 10000}
+      );
+    });
+  }
+  updatePostion(position) {
+    var latitude = position.coords.latitude;
+    var longitude=position.coords.longitude;
+    this.setState({latitude:latitude,longitude:longitude});
+  }
+
   render() {
     return (
       <div className="mt-7 mx-6">
@@ -13,8 +39,8 @@ class ServicesForm extends React.Component {
           </div>
         </Slide>
         <div className="trak_heading-small section mt-3 mb-3">Local Dog Supply Stores:</div>
-        <div className="contact-box my-3 mx-5 px-3 py-5"></div>
-        <div className="contact-box my-3 mx-5 px-3 py-5"></div>
+        <div className="contact-box my-3 mx-5 px-3 py-5">{this.state.latitude}</div>
+        <div className="contact-box my-3 mx-5 px-3 py-5">{this.state.longitude}</div>
         <div className="contact-box my-3 mx-5 px-3 py-5"></div>
         <div className="contact-box my-3 mx-5 px-3 py-5"></div>
         <div className="trak_heading-small section mt-3 mb-3">Local Dog Grooming Services:</div>
